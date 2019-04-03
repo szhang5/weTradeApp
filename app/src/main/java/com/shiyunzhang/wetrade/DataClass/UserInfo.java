eaa27bc13527a083d49548e101d15064f4751a04
ShiyunZhang <shiyun.zhang.zoey@gmail.com>
2019-04-03 15:48:05 -0400

    added editprofile page layout

---
+++ app/src/main/java/com/shiyunzhang/wetrade/DataClass/UserInfo.java
@@ -0,0 +1,112 @@
+package com.shiyunzhang.wetrade.DataClass;
+
+public class UserInfo {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private String address1;
+    private String address2;
+    private String city;
+    private String state;
+    private long zipCode;
+    private String gender;
+
+
+
+    public UserInfo(String firstName, String lastName, String email, String phone, String address1, String address2, String city, String state, long zipCode, String gender) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+        this.address1 = address1;
+        this.address2 = address2;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.gender = gender;
+    }
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public long getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(long zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
--- app/src/main/java/com/shiyunzhang/wetrade/ProfileActivity.java
+++ app/src/main/java/com/shiyunzhang/wetrade/ProfileActivity.java
@@ -4,23 +4,30 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.shiyunzhang.wetrade.Authentication.LoginActivity;
+import com.shiyunzhang.wetrade.DataClass.UserInfo;
 
 public class ProfileActivity extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
-    private TextView mTextView;
+
+    private DatabaseReference databaseReference;
+    private EditText editFirstName, editLastName, editEmail, editPhone, editAddress1, editAddress2, editCity, editState, editZipcode;
+    private Switch editGender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextView = findViewById(R.id.user_email);
         firebaseAuth = FirebaseAuth.getInstance();
 
         if(firebaseAuth.getCurrentUser() == null){
@@ -28,8 +35,16 @@ public class ProfileActivity extends AppCompatActivity {
             startActivity(new Intent(ProfileActivity.this, LoginActivity.class));
         }
 
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        init();
         FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
-        mTextView.setText("Welcome, " + firebaseUser.getEmail());
+        editEmail.setText(firebaseUser.getEmail());
+        findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                saveUserInformation();
+            }
+        });
 
         findViewById(R.id.log_out_button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,6 +53,39 @@ public class ProfileActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void init(){
+        editFirstName = findViewById(R.id.user_firstname_input);
+        editLastName = findViewById(R.id.user_lastname_input);
+        editEmail = findViewById(R.id.user_email_input);
+        editPhone = findViewById(R.id.user_phone_input);
+        editAddress1 = findViewById(R.id.user_address1_input);
+        editAddress2 = findViewById(R.id.user_address2_input);
+        editCity = findViewById(R.id.user_city_input);
+        editState = findViewById(R.id.user_state_input);
+        editZipcode = findViewById(R.id.user_zipcode_input);
+        editGender = findViewById(R.id.sexSwitch);
+    }
+
+//    public void  saveUserInformation(){
+//        String firstName = editFirstName.getText().toString().trim();
+//        String lastName = editLastName.getText().toString().trim();
+//        String email = editEmail.getText().toString().trim();
+//        String phone = editPhone.getText().toString().trim();
+//        String address1 = editAddress1.getText().toString().trim();
+//        String address2 = editAddress2.getText().toString().trim();
+//        String city = editCity.getText().toString().trim();
+//        String state = editState.getText().toString().trim();
+//        long zipCode = Long.parseLong(editZipcode.getText().toString().trim());
+//        String gender = editGender.isChecked()? "Female" : "Male";
+//
+//        UserInfo userInfo = new UserInfo(firstName, lastName, email, phone, address1, address2, city, state, zipCode,gender);
+//        FirebaseUser user = firebaseAuth.getCurrentUser();
+//        databaseReference.child(user.getUid()).setValue(userInfo);
+//
+//        Toast.makeText(ProfileActivity.this, "Profile Information Saved", Toast.LENGTH_LONG).show();
+//    }
+
 
     public void logOut(){
         firebaseAuth.signOut();
