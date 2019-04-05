cfed70222d1e8a8bc866aa14e5b0e20df7a0b346
ShiyunZhang <shiyun.zhang.zoey@gmail.com>
2019-04-04 22:18:22 -0400

    add login action to mainActivity

--- app/src/main/java/com/shiyunzhang/wetrade/MainActivity.java
+++ app/src/main/java/com/shiyunzhang/wetrade/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.shiyunzhang.wetrade.Authentication.LoginActivity;
+import com.shiyunzhang.wetrade.Authentication.RegisterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,31 +24,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_sign_in);
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
 
         progressDialog = new ProgressDialog(this);
-        FirebaseApp.initializeApp(this);
         firebaseAuth = FirebaseAuth.getInstance();
 
         if(firebaseAuth.getCurrentUser() != null){
             finish();
-            startActivity(new Intent(MainActivity.this, HomeActivity.class));
+            startActivity(new Intent(this, HomeActivity.class));
         }
 
-        emailInput = findViewById(R.id.email_input_register);
-        passwordInput = findViewById(R.id.password_input_register);
+        emailInput = findViewById(R.id.email_input);
+        passwordInput = findViewById(R.id.password_input);
 
         addButtonListener();
-
     }
 
-
-    public void registerUser() {
+    public void userLogIn(){
         String email = emailInput.getText().toString();
         String password = passwordInput.getText().toString();
+
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(this, "Please enter email address", Toast.LENGTH_LONG).show();
             return;
@@ -57,26 +56,31 @@ public class MainActivity extends AppCompatActivity {
             return;
         }
 
-        progressDialog.setMessage("Registering User...");
+        progressDialog.setMessage("User signing in...");
         progressDialog.show();
 
-        firebaseAuth.createUserWithEmailAndPassword(email, password)
+        firebaseAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, task -> {
                     progressDialog.dismiss();
                     if (task.isSuccessful()) {
                         finish();
-                        startActivity(new Intent(MainActivity.this, HomeActivity.class));
+                        startActivity(new Intent(this, HomeActivity.class));
                     } else {
                         // If sign in fails, display a message to the user.
-                        Toast.makeText(MainActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(this, "Authentication failed.", Toast.LENGTH_SHORT).show();
                     }
                 });
+
+
     }
 
+    public void addButtonListener(){
+        findViewById(R.id.sign_up).setOnClickListener(v -> {
+            finish();
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
 
-    public void addButtonListener() {
-        findViewById(R.id.sign_up_button).setOnClickListener(v -> registerUser());
-
-        findViewById(R.id.sign_in).setOnClickListener(v -> startActivity(new Intent(MainActivity.this, LoginActivity.class)));
+        findViewById(R.id.sign_in_button).setOnClickListener(v -> userLogIn());
     }
 }
