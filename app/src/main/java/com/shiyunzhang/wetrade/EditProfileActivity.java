cb50ed9e0466ac540a24fbadfa6e4be483e1a783
ShiyunZhang <shiyun.zhang.zoey@gmail.com>
2019-04-11 17:56:25 -0400

    collect user with user profile and user profile image with firebase

--- app/src/main/java/com/shiyunzhang/wetrade/EditProfileActivity.java
+++ app/src/main/java/com/shiyunzhang/wetrade/EditProfileActivity.java
@@ -6,6 +6,7 @@ import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -13,21 +14,28 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.SetOptions;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.shiyunzhang.wetrade.Authentication.LoginActivity;
 import com.shiyunzhang.wetrade.DataClass.UserInfo;
 
@@ -44,6 +52,7 @@ public class EditProfileActivity extends AppCompatActivity {
     private String uid;
     private Uri imageUri;
     private String profileImageUrl;
+    private ProgressBar mProgressBar;
 
     private EditText editFirstName, editLastName, editEmail, editCollege, editGraduationDate, editAddress, editCity, editState, editZipcode;
     private Switch editGender;
@@ -55,9 +64,10 @@ public class EditProfileActivity extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference userRef = db.collection("User");
 
+    //    private DocumentReference userRef = db.collection("User").document();
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch(item.getItemId()){
+        switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
                 return true;
@@ -102,6 +112,7 @@ public class EditProfileActivity extends AppCompatActivity {
     }
 
     public void init() {
+        mProgressBar = findViewById(R.id.profile_progressbar);
         profileImage = findViewById(R.id.profile_image);
         male = findViewById(R.id.male_selector);
         female = findViewById(R.id.female_selector);
@@ -117,6 +128,46 @@ public class EditProfileActivity extends AppCompatActivity {
         editGender = findViewById(R.id.sexSwitch);
     }
 
+    private boolean isValidInput() {
+        if (editFirstName.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please enter your first name", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editLastName.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please enter your last name", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editEmail.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please enter your email", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editCollege.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please enter your college", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editGraduationDate.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please enter your expected graduation date", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editAddress.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please enter your address", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editCity.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please enter the city", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editState.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please enter state", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (editZipcode.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Please enter zip code", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
     public void setUpButtonListener() {
         editGender.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
@@ -127,36 +178,39 @@ public class EditProfileActivity extends AppCompatActivity {
                 male.setTextColor(Color.parseColor("#36B88B"));
             }
         });
-        findViewById(R.id.save_button).setOnClickListener(v -> saveUserInformation());
-        findViewById(R.id.upload_profile).setOnClickListener(v -> {
-            Toast.makeText(this, "clicked save profile image!!!", Toast.LENGTH_LONG).show();
-            uploadFile();
+        findViewById(R.id.save_button).setOnClickListener(v -> {
+            saveUserInformation();
+            Toast.makeText(EditProfileActivity.this, "Profile Information Saved", Toast.LENGTH_LONG).show();
         });
         floatingActionButton = findViewById(R.id.choose_image);
         floatingActionButton.setOnClickListener(v -> openFileChooser());
     }
 
     public void saveUserInformation() {
-        String firstName = editFirstName.getText().toString().trim();
-        String lastName = editLastName.getText().toString().trim();
-        String email = editEmail.getText().toString().trim();
-        String college = editCollege.getText().toString().trim();
-        String graduationDate = editGraduationDate.getText().toString().trim();
-        String address = editAddress.getText().toString().trim();
-        String city = editCity.getText().toString().trim();
-        String state = editState.getText().toString().trim();
-        long zipCode = Long.parseLong(editZipcode.getText().toString().trim());
-        String gender = editGender.isChecked() ? "Female" : "Male";
+        if (isValidInput()) {
+            String firstName = editFirstName.getText().toString().trim();
+            String lastName = editLastName.getText().toString().trim();
+            String email = editEmail.getText().toString().trim();
+            String college = editCollege.getText().toString().trim();
+            String graduationDate = editGraduationDate.getText().toString().trim();
+            String address = editAddress.getText().toString().trim();
+            String city = editCity.getText().toString().trim();
+            String state = editState.getText().toString().trim();
+            long zipCode = Long.parseLong(editZipcode.getText().toString().trim());
+            String gender = editGender.isChecked() ? "Female" : "Male";
 
-        UserInfo userInfo = new UserInfo(firstName, lastName, email, college, graduationDate, address, city, state, zipCode, gender, uid, profileImageUrl);
+            UserInfo userInfo = new UserInfo(firstName, lastName, email, college, graduationDate, address, city, state, zipCode, gender, uid, profileImageUrl);
 
-        userRef.add(userInfo)
-                .addOnSuccessListener(aVoid -> Toast.makeText(EditProfileActivity.this, "Profile Information Saved", Toast.LENGTH_LONG).show())
-                .addOnFailureListener(e -> {
-                    Toast.makeText(EditProfileActivity.this, "Error!", Toast.LENGTH_LONG).show();
-                    Log.d(TAG, e.toString());
-                });
+            userRef.document(uid).set(userInfo, SetOptions.merge())
+                    .addOnSuccessListener(aVoid -> {
+                    })
+                    .addOnFailureListener(e -> {
+                        Toast.makeText(EditProfileActivity.this, "Error!", Toast.LENGTH_LONG).show();
+                        Log.d(TAG, e.toString());
+                    });
+        }
     }
+
 
     public void handleUserData(UserInfo userInfo) {
         editFirstName.setText(userInfo.getFirstName());
@@ -173,26 +227,27 @@ public class EditProfileActivity extends AppCompatActivity {
         } else {
             editGender.setChecked(true);
         }
-        if(userInfo.getImageUrl().trim().length() != 0){
-            Log.d("aaaaaaaaaa", "user image url: " + userInfo.getImageUrl());
-            profileImage.setImageURI(Uri.parse(userInfo.getImageUrl()));
+        if (userInfo.getImageUrl() != null) {
+            Glide.with(this).load(userInfo.getImageUrl()).into(profileImage);
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
                 && data != null && data.getData() != null) {
             imageUri = data.getData();
-            try {
-                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
-                profileImage.setImageBitmap(bitmap);
-            }
-            catch (IOException e)
-            {
-                e.printStackTrace();
-            }
+            mProgressBar.setVisibility(View.VISIBLE);
+//            try {
+//                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+//                profileImage.setImageBitmap(bitmap);
+//            }
+//            catch (IOException e)
+//            {
+//                e.printStackTrace();
+//            }
+            uploadFile();
         }
     }
 
@@ -203,30 +258,31 @@ public class EditProfileActivity extends AppCompatActivity {
         startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
-    private String getFileExtension(Uri uri){
+    private String getFileExtension(Uri uri) {
         ContentResolver cR = getContentResolver();
         MimeTypeMap mime = MimeTypeMap.getSingleton();
         return mime.getExtensionFromMimeType(cR.getType(uri));
     }
 
-    private void uploadFile(){
-        if(imageUri != null) {
-            Log.d("aaaaaaaaaa", "imageUri: " + imageUri.toString());
-            StorageReference fileReference = mStorageRef.child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+    private void uploadFile() {
+        if (imageUri != null) {
+            StorageReference fileReference = mStorageRef.child(uid + "." + getFileExtension(imageUri));
 
             fileReference.putFile(imageUri)
                     .addOnSuccessListener(taskSnapshot -> {
-                        Toast.makeText(EditProfileActivity.this, "Saved Successfully!", Toast.LENGTH_LONG).show();
-                        fileReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                            @Override
-                            public void onSuccess(Uri uri) {
-                                profileImageUrl = uri.toString();
-                                Log.d("aaaaaaaaaa" , "DownloadURL: " + uri.toString());
-                            }
+                        fileReference.getDownloadUrl().addOnSuccessListener(uri -> {
+                            profileImageUrl = uri.toString();
+                            Handler handler = new Handler();
+                            handler.postDelayed(() -> mProgressBar.setProgress(0), 5000);
+                            saveUserInformation();
+                            mProgressBar.setVisibility(View.GONE);
                         });
                     })
-                    .addOnFailureListener(e -> Toast.makeText(EditProfileActivity.this, e.getMessage(), Toast.LENGTH_LONG).show());
-
+                    .addOnFailureListener(e -> Toast.makeText(EditProfileActivity.this, e.getMessage(), Toast.LENGTH_LONG).show())
+                    .addOnProgressListener(taskSnapshot -> {
+                        double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                        mProgressBar.setProgress((int) progress);
+                    });
         } else {
             Toast.makeText(this, "No file selected", Toast.LENGTH_LONG).show();
         }
