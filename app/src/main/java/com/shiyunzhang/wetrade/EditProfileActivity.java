e6adf6d28c04d270c3f95748a340e5a46fec8a3a
ShiyunZhang <shiyun.zhang.zoey@gmail.com>
2019-04-12 12:37:53 -0400

    add up navigation to edit profile activity

--- app/src/main/java/com/shiyunzhang/wetrade/EditProfileActivity.java
+++ app/src/main/java/com/shiyunzhang/wetrade/EditProfileActivity.java
@@ -66,6 +66,16 @@ public class EditProfileActivity extends AppCompatActivity {
     private CollectionReference userRef = db.collection("User");
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
